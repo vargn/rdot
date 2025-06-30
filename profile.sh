65170c2b3eb106fb962e27dfc62059652e1087fb
vargn <58962530+vargn@users.noreply.github.com>
2025-06-30 23:05:48 +0200

    Create profile.sh

---
+++ profile.sh
@@ -0,0 +1,21 @@
+# ~/.tmp/dotfiles/profile.sh
+
+# add ~/.local/bin to PATH if needed
+[[ ":$PATH:" != *":$HOME/.local/bin:"* ]] && export PATH="$HOME/.local/bin:$PATH"
+
+# source system bashrc and profile
+[ -f ~/.bashrc ] && source ~/.bashrc
+[ -f ~/.profile ] && source ~/.profile
+
+# source bash tab-completion
+if [ -f /etc/bash_completion ]; then
+    source /etc/bash_completion
+elif [ -f /usr/share/bash-completion/bash_completion ]; then
+    source /usr/share/bash-completion/bash_completion
+fi
+
+# source custom functions
+[ -f "$HOME/.tmp/dotfiles/functions.sh" ] && source "$HOME/.tmp/dotfiles/functions.sh"
+
+# make ~/.tmp/dotfiles/rshell-cleanup.sh executable
+chmod +x ~/.tmp/dotfiles/rshell-cleanup.sh
